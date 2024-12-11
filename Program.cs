e598134cf8fcbf9ae4c6b961ba4adef968f1a572
andre <ikoto@et.esiea.fr>
2024-12-11 19:57:23 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    //Variable
+    public  static bool quitGame=false;
+    public static bool PlayerTurn =true;
+
+    public static char[,] board;
+
+    //Fonction 
+
+    static void Main(string[] args)
+    {
+        //bouble de jeu / Game loop
+        while(!quitGame)
+        {
+
+        }
+    }
+}
